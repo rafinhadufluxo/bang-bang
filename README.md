7076e416df70f3eb389fe618ee67982c6c8742f6
mazarafa <arruda.rafa.ra@gmail.com>
2021-02-17 18:10:06 -0400

    adding loose tabs, it remains to integrate music api for searching and making changes to the home.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # bang-bang
+
+https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_fullscreen_video
+https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_search_get
---
+++ js/main.js
@@ -0,0 +1,26 @@
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+/* var modal = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+ */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
