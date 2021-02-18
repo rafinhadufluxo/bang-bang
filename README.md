08b829456cbed6399528b261a92fd12f9528759b
mazarafa <arruda.rafa.ra@gmail.com>
2021-02-18 10:15:06 -0400

    making some repairs on the website

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # bang-bang
 
-https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_fullscreen_video
-https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_search_get
+<p>Random project developed to increase knowledge about front end. </p><br>
+
+<h2>Do you like music or producing content?</h2>
+
+<p>On this random platform, you can write a song lyric and generate a pdf of it and you can still play the piano at will, search for news about the musical world and even download music.</p>
+
+
+
+## Reference 
+
+👉 Piano source code is by [Caroline Gabriel](https://github.com/gabrielcarol). (Accessible : https://codepen.io/gabrielcarol/pen/rGeEbY)
+
+
--- js/main.js
+++ js/main.js
@@ -1,21 +1,35 @@
-var modal = document.getElementById('id01');
+var slideIndex = 1;
+showSlides(slideIndex);
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { 
+        slideIndex = 1 
     }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
 
-/* var modal = document.getElementById('id02');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
- */
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
